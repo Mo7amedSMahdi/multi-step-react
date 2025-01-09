0978635739b07351cde229403c83c32ebc1e7b79
Mo7amedSMahdi <mo7ammed.salah@outlook.com>
2025-01-09 20:26:14 +0300

    ♻️ (slices): remove unused slices (createStepSlice, createSubmitFormSlice, createToggleSubscriptionPlan) to clean up the codebase 🔧 (useStore): update useStore to reflect the removal of unused slices and simplify the state management structure

--- src/stores/useStore.ts
+++ src/stores/useStore.ts
@@ -2,35 +2,15 @@ import { create } from 'zustand';
 import { PersonalInfoSlice } from './slices/createPersonalInfoSlice';
 import { PlanSlice } from './slices/createPlanSlice';
 import { AddOnsSlice } from './slices/createAddOnSlice';
-import { ToggleSubcriptionPlanSlice } from './slices/createToggleSubscriptionPlan';
-import {
-  createPersonalInfoSlice,
-  createPlanSlice,
-  createAddOnSlice,
-  createToggleSubscriptionPlan,
-  createStepSlice,
-  createSubmitFormSlice,
-} from './slices';
-import { StepSlice } from './slices/createStepSlice';
-import { SubmitFormSlice } from './slices/createSubmitFormSlice';
+import { createPersonalInfoSlice, createPlanSlice, createAddOnSlice } from './slices';
 import createPlansListSlice, { PlansListSlice } from './slices/createPlansListSlice';
 
-export const useStore = create<
-  PersonalInfoSlice &
-    PlanSlice &
-    AddOnsSlice &
-    ToggleSubcriptionPlanSlice &
-    StepSlice &
-    SubmitFormSlice &
-    AddOnsSlice &
-    PlansListSlice
->()((...a) => ({
-  ...createPersonalInfoSlice(...a),
-  ...createPlanSlice(...a),
-  ...createAddOnSlice(...a),
-  ...createToggleSubscriptionPlan(...a),
-  ...createStepSlice(...a),
-  ...createSubmitFormSlice(...a),
-  ...createAddOnSlice(...a),
-  ...createPlansListSlice(...a),
-}));
+export const useStore = create<PersonalInfoSlice & PlanSlice & AddOnsSlice & AddOnsSlice & PlansListSlice>()(
+  (...a) => ({
+    ...createPersonalInfoSlice(...a),
+    ...createPlanSlice(...a),
+    ...createAddOnSlice(...a),
+    ...createAddOnSlice(...a),
+    ...createPlansListSlice(...a),
+  })
+);
